e6fd7f8f540839785cbebd31304278091f1f0d56
EGOIST <0x142857@gmail.com>
2017-02-16 14:45:36 +0800

    tweak regexp

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ export default function (
   data = {},
   {delimiters = ['{{', '}}']} = {}
 ) {
-  const re = new RegExp(`${delimiters[0]}(.*?)${delimiters[1]}`, 'g')
+  const re = new RegExp(`${delimiters[0]}([\\s\\S]+?)${delimiters[1]}`, 'g')
 
   return tpl.replace(re, (_, exp) => {
     return new Function('data', `with (data) {return ${exp}}`)(data) // eslint-disable-line no-new-func
